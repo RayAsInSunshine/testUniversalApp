86d021395cac9f2024427598ab55185916725c7e
RinkNet <RinkNet@RinkNets-iMac.local>
2013-02-13 11:53:44 -0500

    demo universal app

---
+++ testUniversalApp/testUniversalApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testUniversalApp
+//
+//  Created by RinkNet on 2/13/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testUniversalAppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testUniversalAppAppDelegate class]));
+    }
+}
---
+++ testUniversalApp/testUniversalApp/testUniversalAppViewController.h
@@ -0,0 +1,13 @@
+//
+//  testUniversalAppViewController.h
+//  testUniversalApp
+//
+//  Created by Raymond Wang on 2/13/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testUniversalAppViewController : UIViewController
+-(IBAction)goToVideoView;
+@end
