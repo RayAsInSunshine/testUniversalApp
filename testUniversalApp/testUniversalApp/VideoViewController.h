3fb1592b813f50a8938eabf28728b33be1a86303
RayAsInSunshine <rwang.developer@gmail.com>
2013-02-13 12:12:41 -0500

    demo app
    This reverts commit 5c98cc16e58d3e8eb390dd8b53213ab1882389ee.

---
+++ testUniversalApp/testUniversalApp/VideoViewController.h
@@ -0,0 +1,15 @@
+//
+//  VideoViewController.h
+//  testUniversalApp
+//
+//  Created by Raymond Wang on 2/13/13.
+//  Copyright (c) 2013 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoViewController : UIViewController
+
+-(IBAction)helloWorld;
+
+@end
